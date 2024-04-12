c69dd5669833fbabdd70843a25e9878812e83a19
IHateWindows <117578473+Somenody@users.noreply.github.com>
2024-04-12 11:02:24 +0200

    not *argv[] but **argv

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include "headers.h"
 
-int main(int argc, char * argv[]) {
+int main(int argc, char **argv) {
     int userint, userint1;
     char signe[4] = {'/', '*', '-', '+'};
     char usersigne;
